ea50f0188bf42856b491dea9c9e84533f7bbcfef
= <=>
2024-12-10 18:50:26 +0100

    refactor: Improve image handling and update file paths in ExperienceView

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -3,13 +3,16 @@ import data from '/data.json'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
+const props = defineProps({
+  exp: String,
+})
 const destination = ref(data.destinations[route.params.id - 1])
 </script>
 <template>
-  <h1>{{ destination.experiences[$route.params.exp].name }}</h1> 
+  <h1>{{ destination.experiences[props.exp].name }}</h1> 
   <div>
-    <img :src="'/images/' + destination.experiences[$route.params.exp].image" alt="desc" />
-    <p>{{ destination.experiences[$route.params.exp].description }}</p>
+    <img :src="'/images/' + destination.experiences[props.exp].image" alt="desc" />
+    <p>{{ destination.experiences[props.exp].description }}</p>
   </div>
 </template>
 <style scoped>
