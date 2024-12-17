99bf50a7eff2c2875a82e4e82e238191a6016daa
= <=>
2024-12-17 16:22:11 +0100

    Refactor MenuCmp. Change url route

--- src/components/MenuCmp.vue
+++ src/components/MenuCmp.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterLink } from 'vue-router'
 import data from '/data.json'
-const url = '../../../country/'
+//const url = '../../../country/'
 </script>
 <template>
   <nav id="nav">
     <RouterLink to="/">Home</RouterLink>
-    <RouterLink v-for="(country, i) in data.destinations" :to="url + country.id" :key="i">{{
+    <RouterLink v-for="(country, i) in data.destinations" :to="{name:'country', params:{id:country.id}}" :key="i">{{
       country.name
     }}</RouterLink>
   </nav>
